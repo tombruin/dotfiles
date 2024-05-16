f04f57e85001f2bd2d4e5c49bf8642b6aff2fd0d
tombruin <tombruin26@hotmail.com>
2024-05-16 10:05:18 +0200

    Initial commit of install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Install fonts for zsh
+cd ~
+mkdir .fonts
+cd .fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.tar.xz
+tar -xf FiraCode.tar.xz
+rm FiraCode.tar.xz
+
+# oh-my-zsh & plugins
+apt update
+apt install zsh -y
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+zsh -c 'git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search'
+
+
+sed -i '1 i\HISTFILE=/commandhistory/.zsh_history' ~/.zshrc
+sed -i 's/\(^plugins=([^)]*\)/\1 zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search/' ~/.zshrc
+
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# update theme
+sed -i '/^ZSH_THEME/c\ZSH_THEME="spaceship"' ~/.zshrc
